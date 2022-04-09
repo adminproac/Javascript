1857f4377d345686c85076bbd2142d8b9fefd7bf
Jesus Edgar Duarte Caceres <jesus_edgar_duarte_caceres@hotmail.com>
2022-04-08 20:55:49 -0500

    Constantes y Variables

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,12 @@
+// Variable y constantes
+
+const nombres = 'Jesus Edgar'
+let apellidos = 'Duarte Caceres'
+
+console.log(nombres,apellidos)
+if (true) {
+    let apellidos = 'Duarte Echeverri'
+    console.log(nombres,apellidos)
+}
+
+console.log(nombres,apellidos)
