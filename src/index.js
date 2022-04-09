674671c4691ed6623041647f6bf277009c5a87c6
Jesus Edgar Duarte Caceres <jesus_edgar_duarte_caceres@hotmail.com>
2022-04-09 17:16:11 -0500

    Import, Extpor y uso de algunas funciones importantes de javascrip

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
-//Desestructuracion de arreglos
+//Import, Export y funciones comunes de arreglos
 
-const hermanos = ['william','rocio','edgar','doris','henry']
-const [,,medio] =hermanos
-const [hermanomenor, hermanamenor, yo ,hermanamayor,hermanomayor]=hermanos
+import { heroes } from "./data/heroes";
+console.log(heroes)
 
-console.log(hermanos)
-console.log(medio)
-console.log(hermanomenor,hermanamenor,yo,hermanamayor,hermanomayor)
+//para seleccionar un elemento especifico de la lista
+const getHeroeById=(id)=>(heroes.find((heroe)=>(heroe.id===id?true:false)))
+console.log(getHeroeById(3))
 
+//para listar todos los elementos de la lista
+const getHeroesmap=()=>(heroes.map((heroe)=>(console.log(heroe))))
+console.log(getHeroesmap())
 
-const getState=(valor)=> {
-    return [valor,()=>{console.log(`Se supone que asigno ${valor} a una variable`)}]
-}
+const getHeroesforEach=()=>(heroes.forEach((heroe)=>(console.log(heroe))))
+console.log(getHeroesforEach())
 
-const [nombre,setNombre] = getState('marcos')
+//para filtrar elementos
+const getHeroesByfilter=(id)=>(heroes.filter((heroe)=>(heroe.owner===id)))
+console.log(getHeroesByfilter('mierda'))
 
-console.log(nombre)
-setNombre()
+//Almenos uno
+const getHeroesSome=()=>(heroes.some((heroe)=>(heroe.owner='DC')))
+console.log(getHeroesSome())
+
+//Cumplen condicion
+const getHeroesEvery=()=>(heroes.every((heroe)=>(heroe.id<6)))
+console.log(getHeroesEvery())
+
+//caracteres dentro de cadenas
+const getHeroesFilterIncludes=(cadena)=>(heroes.filter((heroe)=>(heroe.name.includes(cadena))))
+console.log(getHeroesFilterIncludes('man'))
