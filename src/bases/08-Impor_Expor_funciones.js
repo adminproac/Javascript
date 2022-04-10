1c9eb19a0c1812a4f8bb53c660dd3eda0c11ed0c
Jesus Edgar Duarte Caceres <jesus_edgar_duarte_caceres@hotmail.com>
2022-04-09 22:05:30 -0500

    Promesas

--- src/bases/08-Impor_Expor_funciones.js
+++ src/bases/08-Impor_Expor_funciones.js
@@ -1,31 +1,33 @@
 //Import, Export y funciones comunes de arreglos
 
-import { heroes } from "./data/heroes";
-console.log(heroes)
+import { heroes } from "../data/heroes";
+// console.log(heroes)
 
 //para seleccionar un elemento especifico de la lista
-const getHeroeById=(id)=>(heroes.find((heroe)=>(heroe.id===id?true:false)))
-console.log(getHeroeById(3))
+export const getHeroeById=(id)=>(heroes.find((heroe)=>(heroe.id===id?true:false)))
+// console.log(getHeroeById(3))
 
-//para listar todos los elementos de la lista
-const getHeroesmap=()=>(heroes.map((heroe)=>(console.log(heroe))))
-console.log(getHeroesmap())
+// //para listar todos los elementos de la lista
+export const getHeroesmap=()=>(heroes.map((heroe)=>(console.log(heroe))))
+// console.log(getHeroesmap())
 
-const getHeroesforEach=()=>(heroes.forEach((heroe)=>(console.log(heroe))))
-console.log(getHeroesforEach())
+export const getHeroesforEach=()=>(heroes.forEach((heroe)=>(console.log(heroe))))
+// console.log(getHeroesforEach())
 
-//para filtrar elementos
-const getHeroesByfilter=(id)=>(heroes.filter((heroe)=>(heroe.owner===id)))
-console.log(getHeroesByfilter('mierda'))
+// //para filtrar elementos
+export const getHeroesByfilter=(id)=>(heroes.filter((heroe)=>(heroe.owner===id)))
+// console.log(getHeroesByfilter('mierda'))
 
-//Almenos uno
-const getHeroesSome=()=>(heroes.some((heroe)=>(heroe.owner='DC')))
-console.log(getHeroesSome())
+// //Almenos uno
+export const getHeroesSome=()=>(heroes.some((heroe)=>(heroe.owner='DC')))
+// console.log(getHeroesSome())
 
-//Cumplen condicion
-const getHeroesEvery=()=>(heroes.every((heroe)=>(heroe.id<6)))
-console.log(getHeroesEvery())
+// //Cumplen condicion
+export const getHeroesEvery=()=>(heroes.every((heroe)=>(heroe.id<6)))
+// console.log(getHeroesEvery())
 
-//caracteres dentro de cadenas
-const getHeroesFilterIncludes=(cadena)=>(heroes.filter((heroe)=>(heroe.name.includes(cadena))))
-console.log(getHeroesFilterIncludes('man'))
+// //caracteres dentro de cadenas
+export const getHeroesFilterIncludes=(cadena)=>(heroes.filter((heroe)=>(heroe.name.includes(cadena))))
+// console.log(getHeroesFilterIncludes('man'))
+
+ export default getHeroeById
